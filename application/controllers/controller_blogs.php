3e294b6e7e81e96e2468864fdd7dcb862251e25c
Krevii <98155172+Krevii@users.noreply.github.com>
2023-04-21 05:39:51 +0600

    fix

--- application/controllers/controller_blogs.php
+++ application/controllers/controller_blogs.php
@@ -13,18 +13,6 @@ class Controller_Blogs extends Controller
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
-
-
-        $json_data = file_get_contents("php://input");
-        $data = json_decode($json_data, true);
-
-        if (isset($dataT['search'])) {
-            echo $data['search'];
-            // Выводим ошибку для отладки (в реальном приложении лучше использовать логирование)
-            echo "Ошибка: отсутствуют данные 'search' или переменная \$data не содержит массив.";
-            // Бросаем исключение с сообщением об ошибке
-            throw new Exception("Ошибка: отсутствуют данные 'search' или переменная \$data не содержит массив.");
-        }
         
         $search = null;
         if (isset($_POST["search"])) {
@@ -55,12 +43,12 @@ class Controller_Blogs extends Controller
 
                 <div class='forum-footer'>
                     <div class='forum-footer-view'>
-                        <img class='social-icon' src='../../image/like.png' alt='social-icon'>
-                        <span>0</span>
+                        <simg class='social-icon' src='../../image/like.png' alt='social-icon'>
+                        <span></span>
                     </div>
                     <div class='forum-footer-like'>
-                        <img class='social-icon' src='../../image/eye.png' alt='social-icon'>
-                        <span>0</span>
+                        <simg class='social-icon' src='../../image/eye.png' alt='social-icon'>
+                        <span></span>
                     </div>
                 </div>
             </a>"
